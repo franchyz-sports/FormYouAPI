d189270f33fd051dfe47379b353f364c73ca43bf
Floury-Fabio <floury-fabio@hotmail.fr>
2020-06-09 21:01:30 +0200

    table form fix

--- db/migrate/20200619082254_create_formations.rb
+++ db/migrate/20200619082254_create_formations.rb
@@ -3,20 +3,13 @@ class CreateFormations < ActiveRecord::Migration[6.0]
     create_table :formations do |t|
       t.string :title
       t.text :description
+
       t.belongs_to :teacher
       t.belongs_to :creator, index: true, foreign_key: {to_table: :admins}
+      #t.belongs_to :category, index: true
  
       t.timestamps
     end
 
-    # create_table :formations_categories, id: false do |t|
-    #   t.belongs_to :formation
-    #   t.belongs_to :category
-    # end
-
-    create_table :formations_students, id: false do |t|
-      t.belongs_to :formation
-      t.belongs_to :student
-    end
   end
 end
