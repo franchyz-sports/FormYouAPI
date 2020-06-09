7b4e8a89778d3bbb77b473e00065cfb3a83f461c
Floury-Fabio <floury-fabio@hotmail.fr>
2020-06-09 22:21:03 +0200

    setup seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+require 'facker'
+
+Admin.destroy_all
+Teacher.destroy_all
+Student.destroy_all
+
+puts "Desctruction of BDD done"
+password = 'qwerty'
+
+5.times do
+  Admin.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: password, birthdate: Faker::Date.birthday(min_age: 18, max_age: 65), gender: Faker::Gender.short_binary_type, address: Faker::Address.street_address, city: Faker::Address.city,zip_code: Faker::Address.zip_code)
+end
