c59056b3214fb85b50716e3d76a656b8ef8ca8d2
Floury-Fabio <floury-fabio@hotmail.fr>
2020-06-09 14:00:30 +0200

    config session.rb 1/2

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,8 @@
 class Session < ApplicationRecord
+
+  #belongs_to :formation
+  #belongs_to :creator, class_name: 'Admin'
+
+  #has_many :attendances
+  #has_many :students, through: :attendances 
 end
