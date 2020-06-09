332ab5bebf46ebb4f94c0a826d0098cc6a2e3b66
Dim <dimitri.kiavue@gmail.com>
2020-06-09 18:07:04 +0200

    [ADD] Attendance Resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :attendances
+  
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
   devise_for :teachers, controllers: { sessions: 'teachers/sessions' }
   devise_for :students, controllers: { sessions: 'students/sessions' }
@@ -9,6 +11,6 @@ Rails.application.routes.draw do
 
 
 
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
