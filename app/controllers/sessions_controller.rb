4a02375a8481c8966290c61b8e9524c976f25507
Dim <dimitri.kiavue@gmail.com>
2020-06-10 09:25:15 +0200

    [UPDATE] Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,26 @@
 class SessionsController < ApplicationController
-  def create
-    @session = Session.new(formation_params)
-    if admin_signed_in?
-      if @session.save
-        render json: @session, status: :created, location: @session
-      else
-        render json: @session.errors, status: :unprocessable_entity
-      end
-    else
-      "ERREUR"
-    end
+  def show
+   @session = Session.find(params[:id])
+   render json: @session
+ end
 
-  end
+ def create
+   @session = Session.new(session_params)
+   if admin_signed_in?
+     if @session.save
+       render json: @session, status: :created, location: @session
+     else
+       render json: @session.errors, status: :unprocessable_entity
+     end
+   else
+     "ERREUR"
+   end
+ end
+
+private
+
+ def session_params
+   params.require(:session).permit(:max_student, :date, :formation_id, :creator_id)
+ end
+
 end
