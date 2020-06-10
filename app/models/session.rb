e803cc0456ae7119fe9697441dc2e685374a63b3
Floury-Fabio <floury-fabio@hotmail.fr>
2020-06-10 11:29:34 +0200

    model my sessions

--- app/models/session.rb
+++ app/models/session.rb
@@ -8,4 +8,16 @@ class Session < ApplicationRecord
   belongs_to :formation
   belongs_to :creator, class_name: 'Admin'
 
+  self.get_my_sessions(params)
+  
+    if is_teacher?
+      answer = Session.joins(:formation).where("teacher_id = ?", params.id)
+    else if is_student?
+      answer = Session.joins(:formation).where(student_id: params.id)
+    else
+      answer = "dw"
+    end
+
+    answer
+  end
 end
