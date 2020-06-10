c857674668aac54a9c7026bea8fbff3dcba6e35a
Floury-Fabio <floury-fabio@hotmail.fr>
2020-06-10 22:00:46 +0200

    sessions rework done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < ApplicationController
   before_action :set_session, only: [:show, :edit, :update, :destroy]
 
+  before_action :check_auth, only: [:my_sessions, :all_sessions]
+  before_action :decode_token, only: [:my_sessions, :all_sessions]
+  before_action :check_admin, only: [:all_sessions]
+
+  #before_action :check_auth, except: [:index]
+  #before_action :decode_token, except: [:index]
+  #before_action :admin, only: [:all_sessions, :create, :edit, :update]
+
   def show 
     render json: @session
   end
@@ -11,50 +19,36 @@ class SessionsController < ApplicationController
   end
 
   def all_sessions
-    if is_admin?
-      render json: @sessions = Session.all
-    else
-      render 'error'
-    end
+    render json: @sessions = Session.all
   end
 
   def my_sessions
-    id = Teacher.all.sample.id
-    @sessions = Session.get_my_sessions(id)
+    id = decode_token[0]['sub']
+    type = decode_token[0]['scp']
+    @sessions = Session.get_my_sessions(id, type)
+    puts @sessions
     render json: @sessions
   end
 
   def create
     @session = Session.new(session_params)
-    if is_admin?
       if @session.save
         render json: @session, status: :created, location: @session
       else
         render json: @session.errors, status: :unprocessable_entity
       end
-    else
-      "ERREUR"
-    end
   end
 
   def edit
-    if is_admin?
-      render json: @session
-    else
-      render 'error'
-    end
+    render json: @session
   end
 
   def update
-    if is_admin?
       if @session.update(session_params)
         render json: @session, status: :updated, location: @session
       else
         render json: @session.errors, status: :unprocessable_entity
       end
-    else
-      "ERREUR"
-    end
   end
 
 
@@ -67,5 +61,22 @@ class SessionsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def formation_params
       params.require(:session).permit(:max_student, :date)
+    end
+
+    def decode_token
+      token = request.headers['Authorization'].split[1]
+      decoded_token = JWT.decode token, nil, false
+    end
+
+    def check_auth
+      if (request.headers['Authorization'] == nil)
+        render json: 'token is missing'
+      end
+    end
+
+    def check_admin
+      if decode_token['scp'] != 'admin'
+        render json: 'you are not an admin'
+      end
     end
 end
