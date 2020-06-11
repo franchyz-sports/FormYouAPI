0bbb74c26ecba3407ba9a560f5274573bd1594dd
PabloSKZ <pablo.eskenazi@gmail.com>
2020-06-11 13:14:29 +0200

    mailer ok

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -6,9 +6,9 @@ class AdminMailer < ApplicationMailer
     @admin = admin 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
+    @url  = 'https://form-you77.herokuapp.com/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @admin.email, subject: 'Bienvenue chez nous !') 
+    mail(to: @admin.email, subject: 'Bienvenue chez FormYou !') 
   end
 end
--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -6,9 +6,9 @@ class TeacherMailer < ApplicationMailer
     @teacher = teacher 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
+    @url  = 'https://form-you77.herokuapp.com/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @teacher.email, subject: 'Bienvenue chez nous !') 
+    mail(to: @teacher.email, subject: 'Bienvenue chez FormYou !') 
   end
 end
